9c6038452efc7ec0a9738c9ae6f870b33ded0854
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-03-30 16:54:59 +0900

    0330 - Querydsl 4 (advanced)

--- querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -18,6 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -101,6 +103,38 @@ class MemberRepositoryTest {
         // then
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+
+    }
+
+    /*************************/
+    // 실무에서는 쓰기 어려운 기능
+    @Test
+    public void querydslPredicateExecutorTest() throws Exception {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 이런 식으로 간단하게 쓸 수는 있지만,
+        // left Join도 안 되고 서비스 클래스가 queryDsl에 의존해야 한다는 단점이 있다.
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1"))
+        );
+
+        for (Member findMember : result) {
+            //findMember = Member(id=3, username=member1, age=10)
+            System.out.println("findMember = " + findMember);
+        }
 
     }
 
